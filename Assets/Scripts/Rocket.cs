99eb51cf073ebb9d970219b3508435156b99775e
Stuart Gannon <stuart_gannon444@yahoo.co.uk>
2018-05-05 18:02:04 +0100

    Rocket Ship made, started scripting it.

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // Can Thrust while rotating
+        {
+            print("Using Boosters to Thrust");
+        }
+
+        // Press A or D to rotate Left or Right
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating Right");
+        }
+    }
+}
