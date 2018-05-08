3203440183005f6045a2d2eee11569199033cd4d
Stuart Gannon <stuart_gannon444@yahoo.co.uk>
2018-05-08 23:19:15 +0100

    v1.0 Complete

--- Assets/Scripts/FPSDisplay.cs
+++ Assets/Scripts/FPSDisplay.cs
@@ -12,17 +12,20 @@ public class FPSDisplay : MonoBehaviour
 
     void OnGUI()
     {
-        int w = Screen.width, h = Screen.height;
+        if (Debug.isDebugBuild)
+        {
+            int w = Screen.width, h = Screen.height;
 
-        GUIStyle style = new GUIStyle();
+            GUIStyle style = new GUIStyle();
 
-        Rect rect = new Rect(0, 0, w, h * 2 / 100);
-        style.alignment = TextAnchor.UpperLeft;
-        style.fontSize = h * 2 / 25;
-        style.normal.textColor = new Color(255f, 255f, 0.5f, 1.0f);
-        float msec = deltaTime * 1000.0f;
-        float fps = 1.0f / deltaTime;
-        string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
-        GUI.Label(rect, text, style);
+            Rect rect = new Rect(0, 0, w, h * 2 / 100);
+            style.alignment = TextAnchor.UpperLeft;
+            style.fontSize = h * 2 / 25;
+            style.normal.textColor = new Color(255f, 255f, 0.5f, 1.0f);
+            float msec = deltaTime * 1000.0f;
+            float fps = 1.0f / deltaTime;
+            string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+            GUI.Label(rect, text, style);
+        }
     }
 }
