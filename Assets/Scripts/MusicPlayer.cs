b8b486ae40273f9722e959b4074982ed12488eaf
Stuart Gannon <stuart_gannon444@yahoo.co.uk>
2018-05-10 02:54:36 +0100

    Added a Music Player. Adjusted volume of music and Ambient SFX as needed.

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    private static MusicPlayer instance = null;
+
+    private void Awake()
+    {
+        // If Music PLayer doesn't exist
+        // instance is set to Music Player
+        // Music Player isn't destroyed on load
+        // If Music Player Exists, it destroys itself
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
