1f1cf0ff472cb3aa5acc96fb81d43703b6562eda
Stuart Gannon <stuart_gannon444@yahoo.co.uk>
2018-07-06 14:23:44 +0100

    Still working on the Start screen

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,9 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
+
+    GameObject lives;
+
+    private void Start()
+    {
+        lives = GameObject.Find("Lives");
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        if (sceneName == "Start")
+        {
+            lives.SetActive(false);
+        }
+        else
+        {
+            lives.SetActive(true);
+        }
+    }
 
     public void StartGame()
     {
